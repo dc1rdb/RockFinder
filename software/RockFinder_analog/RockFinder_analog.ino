583417a199ac6ef8a0a025f2fda41ceeb2f14d10
Robert <dc1rdb@users.noreply.github.com>
2023-11-18 09:12:20 +0100

    Update RockFinder_analog.ino

--- software/RockFinder_analog/RockFinder_analog.ino
+++ software/RockFinder_analog/RockFinder_analog.ino
@@ -7,26 +7,26 @@ https://github.com/OpenGammaProject/Mini-SiD
 and a Wemos D1 Mini ESP8266. This sketch
 is basically a voltage controlled oscillator (VCO)
 from analog input A0 and outputs an audio frequency
-tone to a passive buzzer
+tone to a passive buzzer.
 
-V1.1 - November 12, 2023
+V1.1 - November 18, 2023
 
 Possible future improvements:
-- threshold adjustment via encoder
+- threshold adjustment via rotary encoder
 - automatic threshold depending on background radiation
 
 */
 
 #include <ESP8266WiFi.h>
 
-const uint8_t buzzerPin = 5;    //Wemos Mini pin: D1
+const uint8_t buzzerPin = 5;    //Wemos Mini pin D1 connects to passive buzzer
 const uint16_t THR = 70;        //threshold value to suppress background SiPM noise
 const uint16_t MIN = 100;       //minimum output tone frequency (Hz)
 const uint16_t MAX = 5000;      //maximum output tone frequency (Hz)
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
-  WiFi.mode(WIFI_OFF);          //turn off WIFI to reduce power consumption
+  WiFi.mode(WIFI_OFF);          //turn off WIFI to reduce ESP8266 power consumption
   delay(1000);
 }
 
