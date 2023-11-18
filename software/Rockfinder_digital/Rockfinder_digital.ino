935cf18e0aeb9f5e7d9ab325665bb8fd5911aa80
Robert <dc1rdb@users.noreply.github.com>
2023-11-18 09:02:13 +0100

    Update Rockfinder_digital.ino

--- software/Rockfinder_digital/Rockfinder_digital.ino
+++ software/Rockfinder_digital/Rockfinder_digital.ino
@@ -3,17 +3,23 @@
 Simple detector for gamma radioactive substances,
 utilizing a Mini SiPM Driver (SiD) Board
 https://github.com/OpenGammaProject/Mini-SiD
-and a Wemos D1 Mini ESP8266. This sketch
-integrates digital pulses from the (SiD) Board,
+and a Wemos D1 Mini ESP8266. 
+This is a digital variant which does not require any 
+additional passive components between the Mini-SiD
+and the ESP8266. This sketch directly integrates 
+digital pulses from the (SiD) Board,
 converts them to a proportional audio frequency
-and outputs the tone on a passive buzzer
+and outputs the tone on a passive buzzer.
+
+N.B. work in progress - does not yet perform well
+(compared to the analog variant).
 
 */
 
 #include <ESP8266WiFi.h>
 
-const int interruptPin = 13;   //Wemos D7
-const int buzzerPin = 5;       //Wemos D1
+const int interruptPin = 13;   //Wemos D7 to digital output of Mini-SiD
+const int buzzerPin = 5;       //Wemos D1 to passive buzzer
 const int MIN = 100;           //minimum output tone frequency (Hz)
 const int MAX = 5000;          //maximum output tone frequency (Hz)
 volatile int sipmpulse = 1;
@@ -40,7 +46,7 @@ void handleInterrupt() {
  
 void loop() {
  
-      Serial.println(sipmpulse);
+      Serial.println(sipmpulse);     //only required for debugging
       int outputfrequency = map(sipmpulse, 2, 624, MIN, MAX);
       tone(buzzerPin, outputfrequency);
       delay(20);
