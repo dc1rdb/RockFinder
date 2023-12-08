0af2debbc71dea9fb1365a19b046c7ce1994fe5e
Robert <dc1rdb@users.noreply.github.com>
2023-12-08 10:01:09 +0100

    Update RockFinder_analog.ino
    Encoder latch mode

--- software/RockFinder_analog/RockFinder_analog.ino
+++ software/RockFinder_analog/RockFinder_analog.ino
@@ -8,7 +8,7 @@ is basically a voltage controlled oscillator (VCO)
 from analog input A0 and outputs an audio frequency
 tone to a passive buzzer.
 
-V2.0 - November 19, 2023
+V2.1 - December 8, 2023
 NEW: adjustable threshold via rotary encoder
 (no code changes required if no encoder is connected)
 
@@ -28,14 +28,15 @@ RotaryEncoder by Matthias Hertel
 #define PIN_IN1 D5              //rotary encoder DT
 #define PIN_IN2 D6              //rotary encoder CLK
 
+#define LATCHMODE FOUR3         //LatchMode FOUR3 or TWO03, depending on encoder type
 #define ROTARYSTEPS 5           //step width for threshold adjustment
 #define ROTARYMIN 5             //minimum threshold value
-#define ROTARYMAX 120           //maximum threshold value
+#define ROTARYMAX 200           //maximum threshold value
 
-RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::LATCHMODE);
 
 void setup() {
-//  Serial.begin(115200);         //debug only
+  Serial.begin(115200);         //debug only
   encoder.setPosition(INI / ROTARYSTEPS);
   pinMode(buzzerPin, OUTPUT);
   WiFi.mode(WIFI_OFF);          //turn off WIFI to reduce ESP8266 power consumption
@@ -58,8 +59,8 @@ void loop() {
     THR = ROTARYMAX;
   } 
 
-//  Serial.print(THR);      //debug only
-//  Serial.println();       //debug only
+  Serial.print(THR);      //debug only
+  Serial.println();       //debug only
 
 //output tone to buzzer
   uint16_t frequencyinput = analogRead(A0);
