27df0063134b411d738ce78dda01e96e37623b47
Robert <dc1rdb@users.noreply.github.com>
2024-01-06 18:57:51 +0100

    Cleanup and threshold change

--- software/RockFinder_analog/RockFinder_analog.ino
+++ software/RockFinder_analog/RockFinder_analog.ino
@@ -8,7 +8,7 @@ is basically a voltage controlled oscillator (VCO)
 from analog input A0 and outputs an audio frequency
 tone to a passive buzzer.
 
-V2.1 - December 8, 2023
+V2.2 - January 6, 2024
 NEW: adjustable threshold via rotary encoder
 (no code changes required if no encoder is connected)
 
@@ -22,21 +22,21 @@ RotaryEncoder by Matthias Hertel
 
 #define MIN 100                 //minimum output tone frequency (Hz)
 #define MAX 5000                //maximum output tone frequency (Hz)
-#define INI 70                  //initial threshold value (or fixed threshold if no encoder is connected)
-
 #define buzzerPin D1            //passive buzzer between this pin and GND
+
 #define PIN_IN1 D5              //rotary encoder DT
 #define PIN_IN2 D6              //rotary encoder CLK
 
 #define LATCHMODE FOUR3         //LatchMode FOUR3 or TWO03, depending on encoder type
 #define ROTARYSTEPS 5           //step width for threshold adjustment
 #define ROTARYMIN 5             //minimum threshold value
-#define ROTARYMAX 200           //maximum threshold value
+#define ROTARYMAX 300           //maximum threshold value
+#define INI 90                  //initial threshold value (or fixed threshold if no encoder is connected)
 
 RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::LATCHMODE);
 
 void setup() {
-  Serial.begin(115200);         //debug only
+//  Serial.begin(115200);         //debug only
   encoder.setPosition(INI / ROTARYSTEPS);
   pinMode(buzzerPin, OUTPUT);
   WiFi.mode(WIFI_OFF);          //turn off WIFI to reduce ESP8266 power consumption
@@ -59,8 +59,8 @@ void loop() {
     THR = ROTARYMAX;
   } 
 
-  Serial.print(THR);      //debug only
-  Serial.println();       //debug only
+  //Serial.print(THR);      //debug only
+  //Serial.println();       //debug only
 
 //output tone to buzzer
   uint16_t frequencyinput = analogRead(A0);
